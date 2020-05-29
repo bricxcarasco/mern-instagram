44809771c7e0fcbe313c88b5aee0dd5b823fb0dd
Bricx Carasco <bricxcarasco.gss@gmail.com>
2020-05-29 15:40:15 +0800

    Create all posts api

--- routes/post.js
+++ routes/post.js
@@ -5,6 +5,19 @@ const requireLogin = require('../middleware/requireLogin');
 
 const Post = mongoose.model('Post');
 
+router.get('/allpost', (req, res) => {
+    Post.find()
+        .populate("postedBy", "_id name")
+        .then(posts => {
+            res.json({
+                posts
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        });
+});
+
 router.post('/createpost', requireLogin, (req, res) => {
     const { title, body } = req.body;
     if (!title || !body) {
