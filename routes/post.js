5d2c13d0c4712de8e44efe73bd3ee1b199361927
Bricx Carasco <bricxcarasco.gss@gmail.com>
2020-06-06 15:02:27 +0800

    Create like and unlike api

--- routes/post.js
+++ routes/post.js
@@ -58,4 +58,38 @@ router.post('/createpost', requireLogin, (req, res) => {
         });
 });
 
+router.put('/like', requireLogin, (req, res) => {
+    Post.findByIdAndUpdate(req.body.postId, {
+        $push: {
+            likes: req.user._id
+        }
+    }, {
+        new: true
+    })
+    .exec((error, result) => {
+        if (error) {
+            return res.status(422).json(error);
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+router.put('/unlike', requireLogin, (req, res) => {
+    Post.findByIdAndUpdate(req.body.postId, {
+        $pull: {
+            likes: req.user._id
+        }
+    }, {
+        new: true
+    })
+    .exec((error, result) => {
+        if (error) {
+            return res.status(422).json(error);
+        } else {
+            res.json(result);
+        }
+    });
+});
+
 module.exports = router;
