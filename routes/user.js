0febe7b521b5bb1459e0f609b21d625821a04604
Bricx Carasco <bricxcarasco.gss@gmail.com>
2020-06-16 13:24:28 +0800

    Create change profile pictur api

--- routes/user.js
+++ routes/user.js
@@ -35,6 +35,26 @@ router.get('/user/:id', requireLogin, (req, res) => {
     })
 });
 
+router.put('/change/profile/picture', requireLogin, (req, res) => {
+    User.findByIdAndUpdate(req.user._id, {
+        $set: {
+            photo: req.body.photo
+        }
+    }, {
+        new: true
+    })
+    .exec((error, result) => {
+        if (error) {
+            console.log(error);
+            return res.status(422).json(error);
+        }
+        res.json({
+            message: "Profile picture successfully updated",
+            photo: result.photo
+        });
+    });
+});
+
 router.put('/follow', requireLogin, (req, res) => {
     User.findByIdAndUpdate(req.body.id, {
         $push: {
